9cfac0ada0fbc56d84fede525e105a641eb2fd04
TXT2LRN <query.txt2lrn@gmail.com>
2019-04-06 18:44:04 -0700

    getAddy: now returns tuple; write csv file

--- criis/ref.py
+++ criis/ref.py
@@ -8,6 +8,7 @@ import datetime
 #import json
 import rapidjson
 import sys
+import csv
 
 ## Global
 #url = "https://data.sfgov.org/resource/45et-ht7c.json?mapblklot=" 
@@ -29,8 +30,12 @@ def getAddy(apn):
 			print(apn, ",NA", sep='')
 			return
 		a = d[0] #address info
-		#print(apn,",",a['from_st'], a['street'], a['st_type'])
-		print(apn,",",a['from_st'], a['street'])
+		c = d[1]['geometry']['coordinates'] # coordinate list
+		pt = c[0][0] # first coordinate
+		#print(apn,",",a['from_st']," ",a['street'],",",pt[0],",",pt[1], sep='') #a['st_type'])
+		s = (apn,a['from_st']+" "+a['street'],pt[0],pt[1])
+		print('.', end='', flush=True) #print(s)
+		return s
 
 #print('Begin Parse:', getTime())
 #getAddy("3624001")
@@ -40,16 +45,25 @@ def getAddy(apn):
 
 print('Begin Parse:', getTime())
 apns = open('apn.txt', "r").read().splitlines()
+addyList = []
 for apn in apns:
-	getAddy(apn)
-print('End   Parse:', getTime())
+	a = getAddy(apn)
+	addyList.append(a)
+print('\nEnd   Parse:', getTime())
  
 # output addresses
-#f = open('addy.txt', 'w')
-#for apn in apn_set:
-#	f.write(apn)
-#	f.write('\n')
-#f.close()
-#print("\napn.txt written:", len(apn_set))
+# https://stackoverflow.com/questions/15578331/save-list-of-ordered-tuples-as-csv
+with open('a.csv', 'w') as f:
+	fw = csv.writer(f) #fw: file writer
+	fw.writerow(['apn','addy','long','lat'])
+	for addy in addyList:
+		fw.writerow(addy)
+f.close()	
+print("\na.csv written:", len(addyList))
 
 sys.exit()
+
+# https://thefrontsteps.com/2016/08/01/bay-area-liquefaction-landslide-and-seismic-zones-mapped/
+# http://urbanspatialanalysis.com/dataviz-tutorial-mapping-san-francisco-home-prices-using-r/
+# http://simonkassel.com/blog/2017/2/21/mapping-and-visualizing-san-franciscos-residential-real-estate-market-with-r
+# https://www.arcgis.com/home/item.html?id=2d5b69b44e4c4f1b90bafcae9b823c17
