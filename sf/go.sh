0a4f9700841a89b0b8b0116149feb052241bd7b0
TXT2LRN <query.txt2lrn@gmail.com>
2019-12-01 23:23:44 -0800

    merge / dedup the APNs

--- sf/go.sh
+++ sf/go.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # defaults: http://linuxcommand.org/lc3_wss0120.php
+# if a file is passed in parse it
 if [ "$1" != "" ]; then
   inputfile=$1
+# otherwise run node/puppeteer to get nod.html 
 else
-  echo "[error] go.sh <criis-html>"
-  exit
+  node criis.js >nod.html
+  inputfile=nod.html
 fi
 
 echo "[info] $inputfile being scanned"
@@ -14,12 +16,15 @@ echo "[info] $inputfile being scanned"
 ./nod2apn.py -i $inputfile -o nod_apn.txt
 
 # sort them, stackoverflow.com/questions/29244351
-sort -o nod_apn.txt nod_apn.txt
+#sort -o nod_apn.txt nod_apn.txt
+
+# merge the apn and date
+./merge.py -i nod_apn.txt -o n.csv
 
 # APN to Address & GPS coordinate
 #https://dev.socrata.com/foundry/data.sfgov.org/45et-ht7c
 #https://data.sfgov.org/resource/45et-ht7c.json?mapblklot=3624001
-./apn2geo.py -i nod_apn.txt -o addr.csv
+./apn2geo.py -i n.csv -o addr.csv
 
 # map addr.csv file
 ./map.R addr.csv map.pdf
